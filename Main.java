b574609d68a6d5fc15fa2b7733107b2f7e62c959
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-05-15 15:41:57 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Product product1 = new Product("Nước tương", 8000);
-        Product product2 = new Product("Đường", 18000);
-        Product product3 = new Product("Gạo", 10000);
+        Product product2 = new Product("Gạo", 18000);
+        Product product3 = new Product("Đường", 10000);
+        Product product4 = new Product("Gạo", 18000);
 
         Order order = new Order(1, LocalDate.of(2015, 9, 10));
-        order.addLineItem(product1, 2);
-        order.addLineItem(product2, 1);
-        order.addLineItem(product3, 4);
+        order.addLineItem(product1, 10);
+        order.addLineItem(product2, 5);
+        order.addLineItem(product3, 1);
+        order.addLineItem(product4, 1);
 
         order.printOrderDetails();
     }
